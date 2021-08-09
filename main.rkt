4530c4adf6ad061a72e3d35b4f51bbfeb32755fe
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-08-10 06:14:59 +0800

    form type and constructor

--- main.rkt
+++ main.rkt
@@ -6,17 +6,33 @@
 (require syntax/parse/define
          (for-syntax syntax/parse))
 
+(begin-for-syntax
+  (struct s:type (level) #:transparent)
+  (struct s:constructor (name type) #:transparent)
+
+  (define-syntax-class type
+    (pattern Type
+             #:attr value #`(s:type #,(gensym 'level)))
+    (pattern name:id
+             #:attr value #''name))
+
+  (define-syntax-class constructor
+    (pattern [name:id binds ... : typ:type]
+             #:attr value #`(s:constructor name typ))))
+
 (define-for-syntax (parse stx)
   (syntax-parse stx
-    [(data name:id : typ:id
-           constructor ...)
-     (list #'name #'typ #'(constructor ...))]
+    [(data name:id : typ:type
+           ctors:constructor ...)
+     (displayln #'typ.value)
+     (displayln #'(ctors.value ...))
+     ]
     [else #'else]))
 
 (define-syntax-parser module-begin
   [(statements ...)
    (for ([statement (syntax->list #'(statements ...))])
-     (displayln (parse statement)))
+     (parse statement))
    #'(#%module-begin)])
 
 (module reader syntax/module-reader k)
