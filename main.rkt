08b4622bb5139b7c568cd860f206a31698dad902
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-08-10 06:35:47 +0800

    introduce pi type and telescope

--- main.rkt
+++ main.rkt
@@ -7,21 +7,31 @@
          (for-syntax syntax/parse))
 
 (begin-for-syntax
-  (struct s:type (level) #:transparent)
+  (struct t:type (level) #:transparent)
+  (struct t:pi (tele+ body) #:transparent)
   (struct s:constructor (name type) #:transparent)
+  (struct telescope (name type) #:transparent)
 
   (define-syntax-class type
     (pattern Type
-             #:attr value #`(s:type #,(gensym 'level)))
+             #:attr value #`(t:type #,(gensym 'level)))
     (pattern name:id
              #:attr value #''name))
 
+  (define-syntax-class bind
+    (pattern [name:id : typ:type]
+             #:attr value #`(telescope name typ)))
   (define-syntax-class constructor
-    (pattern [name:id binds ... : typ:type]
-             #:attr value #`(s:constructor name typ))))
+    (pattern [name:id : typ:type]
+             #:attr value #`(s:constructor name typ))
+    (pattern [name:id binds:bind ... : typ:type]
+             #:attr value #`(s:constructor name (t:pi (list binds.value ...) typ)))))
 
 (define-for-syntax (parse stx)
   (syntax-parse stx
+    [(data name:id : typ:type)
+     (displayln #'typ.value)
+     ]
     [(data name:id : typ:type
            ctors:constructor ...)
      (displayln #'typ.value)
