4f1c0a2c2e462d97933ae685b0255db3d1d8c03f
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-08-13 05:34:27 +0800

    fix nested application

--- data/nat.rkt
+++ data/nat.rkt
@@ -6,3 +6,10 @@
 (data Nat : Type
       [z : Nat]
       [s (n : Nat) : Nat])
+
+(module+ test
+  (require rackunit)
+
+  (check-equal? Nat 'Nat)
+  (check-equal? z 'z)
+  (check-equal? (s (s (s z))) '(s (s (s z)))))
--- main.rkt
+++ main.rkt
@@ -16,7 +16,9 @@
     (pattern name:id
              #:attr value (if (eq? (syntax->datum #'name) 'Type)
                               #`(t:type '#,(gensym 'level))
-                              #'name)))
+                              #'name))
+    (pattern (f args ...)
+             #:attr value #'(f args ...)))
 
   (define-syntax-class bind
     (pattern [name:id : typ:type]
@@ -41,29 +43,36 @@
        (define name 'name)
        ctors.define ...)])
 
+(define-for-syntax (core-check stx)
+  (syntax-parse stx
+    [(f args ...)
+     (match (eval #'f)
+       [(struct* t:the ([value value] [type type]))
+        (match type
+          [(struct* t:pi ([tele+ tele+] [body typ]))
+           (define m (make-hash))
+           (define result
+             (for/list ([arg-stx (syntax->list #'(args ...))]
+                        [tele tele+])
+               (match-define (struct* t:the ([value value] [type arg-type]))
+                 (core-check arg-stx))
+               (match-define (struct* telescope ([name name] [type expect-type]))
+                 tele)
+               (unless (equal? expect-type arg-type)
+                 (raise-syntax-error 'type-mismatched "type mismatched"
+                                     arg-stx))
+               (hash-set! m name value)
+               value))
+           (eval #`(t:the '#,(cons value result)
+                          '#,(subst typ m)))]
+          [else (raise-syntax-error 'not-a-function "not a function"
+                                    #'f)])]
+       [else (void)])]
+    [n (eval #'n)]))
+
 (define-syntax-parser k-app
   [(_ f args ...)
-   (match (eval #'f)
-     [(struct* t:the ([value value] [type type]))
-      (match type
-        [(struct* t:pi ([tele+ tele+] [body typ]))
-         (define m (make-hash))
-         (define result
-           (for/list ([arg-stx (syntax->list #'(args ...))]
-                      [arg (eval #'(list args ...))]
-                      [tele tele+])
-             (match-define (struct* t:the ([value value] [type arg-type])) arg)
-             (match-define (struct* telescope ([name name] [type expect-type])) tele)
-             (unless (equal? expect-type arg-type)
-               (raise-syntax-error 'type-mismatched "type mismatched"
-                                   arg-stx))
-             (hash-set! m name value)
-             value))
-         #`(t:the '#,(cons value result)
-                  '#,(subst typ m))]
-        [else (raise-syntax-error 'not-a-function "not a function"
-                                  #'f)])]
-     [else (void)])
+   (core-check #'(f args ...))
    #'(#%app f args ...)])
 
 (module reader syntax/module-reader k)
