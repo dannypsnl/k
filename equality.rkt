5e66dbd59e673e1007104fbff808500fd120fff6
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-08-18 18:54:36 +0800

    test(moving)

--- equality.rkt
+++ equality.rkt
@@ -5,3 +5,26 @@
 
 (data (≡ [a : A] [b : A]) : Type
       [refl : (≡ a a)])
+
+(module+ test
+  (require k/data/bool
+           k/data/nat)
+
+  (check (≡ true true)
+         (refl))
+
+  (check (≡ (+ (s z) (s z))
+            (s (s z)))
+         (refl))
+
+  (check (≡ (s (s z))
+            (+ (s z) (s z)))
+         (refl))
+
+  (check (≡ (* (s z) (s z))
+            (s z))
+         (refl))
+
+  (check (≡ (* (s (s z)) (s (s z)))
+            (s (s (s (s z)))))
+         (refl)))
