de703434123460cc59e095c190d408c4aa782b23
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-08-21 10:52:37 +0800

    lib(equality cong)

--- equality.rkt
+++ equality.rkt
@@ -1,6 +1,12 @@
 #lang k
 
-(provide ≡ refl)
+(provide ≡ refl
+         cong)
 
 (data (≡ [a : A] [b : A]) : Type
       [refl : (≡ a a)])
+
+(def (cong [f : (-> A B)] [x : A] [y : A] 
+           [P : (≡ x y)])
+  : (≡ (f x) (f y))
+  [f x y refl => refl])
