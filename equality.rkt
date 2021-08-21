47fa317d046c775ede9cdb13d84899af7e5e1f4c
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-08-21 22:59:22 +0800

    lib(revert cong)
    The definition is not we expected

--- equality.rkt
+++ equality.rkt
@@ -1,12 +1,6 @@
 #lang k
 
-(provide ≡ refl
-         cong)
+(provide ≡ refl)
 
 (data (≡ [a : A] [b : A]) : Type
       [refl : (≡ a a)])
-
-(def (cong [f : (-> A B)] [x : A] [y : A] 
-           [P : (≡ x y)])
-  : (≡ (f x) (f y))
-  [f x y refl => refl])
