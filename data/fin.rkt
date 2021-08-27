8ccf6bf095d61d8b7e9c87b294cc8e174117c686
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-08-28 04:09:45 +0800

    lib(test data/fin)

--- data/fin.rkt
+++ data/fin.rkt
@@ -7,3 +7,15 @@
 (data (Fin [n : Nat]) : Type
       [fzero : (Fin (s n))]
       [fsuc (x : (Fin n)) : (Fin (s n))])
+
+(module+ test
+  (check (Fin (s z))
+         fzero)
+
+  (check (Fin (s (s z)))
+         fzero)
+  (check (Fin (s (s z)))
+         (fsuc fzero))
+
+  (check (Fin (s (s (s z))))
+         (fsuc (fsuc fzero))))
