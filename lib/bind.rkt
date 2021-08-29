d49524288ab41ff03f0ba1d5421a0e2b0363c3c8
cybai <cyb.ai.815@gmail.com>
2021-08-29 16:10:00 +0900

    bind(share type)
    Implement share type bindings.
    
    Resolve #20

--- lib/bind.rkt
+++ lib/bind.rkt
@@ -2,13 +2,15 @@
 
 (provide bindings)
 
-(require syntax/parse)
+(require syntax/parse
+         syntax/stx)
 
 (define-splicing-syntax-class bindings
   (pattern (~seq b*:bind ...)
-           #:with (name ...) #'(b*.name ...)
-           #:with (ty ...) #'(b*.ty ...)))
+           #:with (name ...) #'(b*.name ... ...)
+           #:with (ty ...) #'(b*.ty ... ...)))
 
 (define-syntax-class bind
   #:datum-literals (:)
-  (pattern (name:id : ty)))
+  (pattern (name:id ... : type)
+           #:with (ty ...) (stx-map (lambda (name) #'type) #'(name ...))))
