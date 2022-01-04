b2c5c9e11be6db23810f679337cf1f1a947d9eb6
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-01-05 01:33:34 +0800

    `k-lib`

--- k/info.rkt
+++ k/info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection 'multi)
-(define deps '("k-core"))
-(define implies '("k-core"))
+(define deps '("k-core" "k-lib"))
+(define implies '("k-core" "k-lib"))
 (define pkg-desc "k theorem prover")
 (define version "0.0")
 (define license '(Apache-2.0 OR MIT))
