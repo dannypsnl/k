fa64430f1651f9d1f4847af73498f553297dd7b6
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-01-05 01:31:35 +0800

    extract `k-core`

---
+++ k-core/info.rkt
@@ -0,0 +1,6 @@
+#lang info
+(define collection 'multi)
+(define deps '("base"))
+(define build-deps '())
+(define pkg-desc "core of k")
+(define pkg-authors '(dannypsnl cybai))
---
+++ k/info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection 'multi)
+(define deps '("k-core"))
+(define implies '("k-core"))
+(define pkg-desc "k theorem prover")
+(define version "0.0")
+(define license '(Apache-2.0 OR MIT))
+(define pkg-authors '(dannypsnl cybai))
