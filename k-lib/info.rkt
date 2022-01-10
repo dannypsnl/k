6682b2de96b8b2b6d2de5a26438c0aba887494d1
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-01-11 06:52:02 +0800

    lib(missing dep to k-core)

--- k-lib/info.rkt
+++ k-lib/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection 'multi)
-(define deps '("base"))
+(define deps '("base" "k-core"))
 (define build-deps '())
 (define pkg-desc "library of k")
 (define pkg-authors '(dannypsnl cybai))
