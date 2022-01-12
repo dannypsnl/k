04c857a2ee66ebf2172ea22e3243eaba6cecde8d
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-01-12 13:36:00 +0800

    lib(need rackunit-lib)

--- k-lib/info.rkt
+++ k-lib/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection 'multi)
 (define deps '("base" "k-core"))
-(define build-deps '())
+(define build-deps '("rackunit-lib"))
 (define pkg-desc "library of k")
 (define pkg-authors '(dannypsnl cybai))
