3c98e467ed933c92e8749743a9d83d8a2741e535
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-03-30 21:58:50 +0800

    lib(merge definitions into `k/equality` module)

--- k-lib/k/equality.rkt
+++ k-lib/k/equality.rkt
@@ -1,6 +1,36 @@
 #lang k/base
-
-(provide (data-out ≡))
+(provide (data-out ≡)
+         #;cong
+         symm
+         trans)
 
 (data (≡ [a b : A]) : Type
       [refl : (≡ a a)])
+
+#|
+TODO:
+1. `cong` is not definable now, since our implementation didn't treat `f` as a procedure now
+2. `A`, `B`, `x`, `y`, `z` should no need pattern(implicit)
+|#
+; Congruence
+#;(def (cong {A B : Type}
+             [f : (Pi ([x : A]) B)]
+             [x y : A]
+             [P : (≡ x y)])
+    : (≡ (f x) (f y))
+    [A B f x y refl => refl])
+
+; Symmetry
+(def (symm {A : Type}
+           [x y : A]
+           [p : (≡ x y)])
+  : (≡ y x)
+  [A x y refl => refl])
+
+; Transitivity
+(def (trans {A : Type}
+            [x y z : A]
+            [p : (≡ x y)]
+            [q : (≡ y z)])
+  : (≡ x z)
+  [A x y z refl refl => refl])
