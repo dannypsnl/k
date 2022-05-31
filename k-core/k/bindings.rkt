b113c74d2d5d1c55a9152e3fb4dfa2ed13183dc3
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-05-31 12:47:30 +0800

    core(update bindings syntax)

--- k-core/k/bindings.rkt
+++ k-core/k/bindings.rkt
@@ -1,5 +1,4 @@
 #lang racket/base
-
 (provide bindings)
 
 (require syntax/parse
@@ -8,24 +7,24 @@
 
 (define-splicing-syntax-class bindings
   (pattern (~seq b*:bind ...)
-           #:with (name ...) #'(b*.explicit-name ... ...)
-           #:with (ty ...) #'(b*.explicit-ty ... ...)
-           #:with (implicit-name ...) #'(b*.implicit-name ... ...)
-           #:with (implicit-ty ...) #'(b*.implicit-ty ... ...)
-           #:with (full-name ...) #'(b*.name ... ...)
-           #:with (full-ty ...) #'(b*.ty ... ...)))
+    #:with (name ...) #'(b*.explicit-name ... ...)
+    #:with (ty ...) #'(b*.explicit-ty ... ...)
+    #:with (implicit-name ...) #'(b*.implicit-name ... ...)
+    #:with (implicit-ty ...) #'(b*.implicit-ty ... ...)
+    #:with (full-name ...) #'(b*.name ... ...)
+    #:with (full-ty ...) #'(b*.ty ... ...)))
 
 (define-syntax-class bind
   #:datum-literals (:)
-  (pattern (~braces name:id ... : type)
-           #:with (ty ...) (stx-map (lambda (name) #'type) #'(name ...))
-           #:with (implicit-name ...) #'(name ...)
-           #:with (implicit-ty ...) #'(ty ...)
-           #:with (explicit-name ...) #'()
-           #:with (explicit-ty ...) #'())
+  (pattern {~braces name:id ... : type}
+    #:with (ty ...) (stx-map (lambda (name) #'type) #'(name ...))
+    #:with (implicit-name ...) #'(name ...)
+    #:with (implicit-ty ...) #'(ty ...)
+    #:with (explicit-name ...) #'()
+    #:with (explicit-ty ...) #'())
   (pattern (name:id ... : type)
-           #:with (ty ...) (stx-map (lambda (name) #'type) #'(name ...))
-           #:with (implicit-name ...) #'()
-           #:with (implicit-ty ...) #'()
-           #:with (explicit-name ...) #'(name ...)
-           #:with (explicit-ty ...) #'(ty ...)))
+    #:with (ty ...) (stx-map (lambda (name) #'type) #'(name ...))
+    #:with (implicit-name ...) #'()
+    #:with (implicit-ty ...) #'()
+    #:with (explicit-name ...) #'(name ...)
+    #:with (explicit-ty ...) #'(ty ...)))
