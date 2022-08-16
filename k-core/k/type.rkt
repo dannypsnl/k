15b85fc240eb48f489829d8a79fede9164a9fb6e
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-08-16 18:29:56 +0800

    core(remove universe level of type for now)
    This is because later we would like to dig into a stable sized type
    theory, this will make huge impact to this part, and we also haven't
    had chance to write wrong code that runs into universe problem. The
    first level mission now is fixing the implicit with complete solution.
    
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- k-core/k/type.rkt
+++ k-core/k/type.rkt
@@ -8,22 +8,9 @@
   #:datum-literals (:)
   [(_ ([tele-name* : tele-typ*] ...)
       result)
-   (define max-level
-     (apply max
-            (cons 0
-                  (filter number?
-                          (map (λ (stx)
-                                 (syntax-property stx 'level))
-                               (syntax->list #'(tele-typ* ...)))))))
    (syntax-property #'`(Pi (,`[tele-name* : ,tele-typ*] ...)
                            ,result)
-                    'type #`(Type #,max-level))])
+                    'type #'Type)])
 
 (define-syntax-parser Type
-  [_ (syntax-property
-      (syntax-property #'(list 'Type 0) 'type #'(Type 1))
-      'level 0)]
-  [(_ n:integer)
-   (syntax-property
-    (syntax-property #'(list 'Type n) 'type #'(Type (add1 n)))
-    'level (syntax->datum #'n))])
+  [_ (syntax-property #''Type 'type #'Type)])
